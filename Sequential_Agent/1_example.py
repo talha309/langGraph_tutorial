2c8db7700a343796d978bc4b7195e18852ceb98e
talha309 <tiseoexpert@gmail.com>
2025-12-01 19:35:24 +0500

    sequential

---
+++ Sequential_Agent/1_example.py
@@ -0,0 +1,29 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+class AgentState(TypedDict):
+    name: str
+    age: str
+    final: str
+
+def first_node(state: AgentState) ->AgentState:
+    """this is first node."""
+    state['final']= f"hi {state['name']},how I can help you!"
+    return state
+def second_node(state: AgentState) -> AgentState:
+    """this is second node."""
+    state['final']= state['final'] + f"You are {state['age']} years old!"
+    return state
+graph = StateGraph(AgentState)
+graph.add_node("FIRST", first_node)
+graph.add_node("SECOND", second_node)
+graph.add_edge(START,"FIRST")
+graph.add_edge("FIRST","SECOND")
+graph.add_edge("SECOND",END)
+
+app = graph.compile()
+result = app.invoke({
+    "name":"ANAS",
+    "age": 34,
+})
+print (result)
